51d92069781105f2db314d731eb31c933d88d79b
Lea Seica <lea.seica@skema.edu>
2024-03-19 14:32:24 +0100

    Add dotenv-rails

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,8 @@
 class Article < ApplicationRecord
   belongs_to :sub_category
+  has_many :my_guides
+  has_many :platforms, through: :articles_plateforms, source: :join_association_table_foreign_key_to_platforms_table
+
+  validates :title, presence: true, uniqueness: true
+
 end
--- app/models/platform.rb
+++ app/models/platform.rb
@@ -1,2 +1,3 @@
 class Platform < ApplicationRecord
+  has_many :articles, through: :articles_plateforms, source: :join_association_table_foreign_key_to_articles_table
 end
