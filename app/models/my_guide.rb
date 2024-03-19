1c54b89e56cb0630620a8fd2118002cb2470fdf2
Lea Seica <lea.seica@skema.edu>
2024-03-19 15:17:02 +0100

    seed setup

--- app/models/my_guide.rb
+++ app/models/my_guide.rb
@@ -2,4 +2,16 @@ class MyGuide < ApplicationRecord
   belongs_to :article
   belongs_to :user
   has_one_attached :photo
+
+  after_update :update_user_occupation
+
+  private
+
+  def update_user_occupation
+    # Check if the occupation attribute was changed during the update
+    if saved_change_to_occupation?
+      # Update the associated user's occupation
+      user.update(occupation: self.occupation)
+    end
+  end
 end
