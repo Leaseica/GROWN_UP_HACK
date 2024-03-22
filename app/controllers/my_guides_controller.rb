08f8affbec7cbb50374481dcddd6a7b9a25b39b2
Lea Seica <lea.seica@skema.edu>
2024-03-22 15:31:31 +0100

    form summary, download pdf

--- app/controllers/my_guides_controller.rb
+++ app/controllers/my_guides_controller.rb
@@ -1,7 +1,9 @@
+require 'prawn'
+
 class MyGuidesController < ApplicationController
-  before_action :set_article, only: %i[new create]
-  before_action :set_user, only: %i[new create]
-  before_action :set_my_guide, only: %i[show edit update destroy]
+  before_action :set_article, only: [:new, :create, :edit, :update]
+  before_action :set_my_guide, only: [:show, :edit, :update, :destroy, :summary]
+  before_action :set_user, only: [:show, :new, :create, :edit, :update, :destroy, :summary]
 
   # GET my_guides/new
   def new
@@ -10,15 +12,19 @@ class MyGuidesController < ApplicationController
 
   # GET my_guides/1
   def show
+    @view_mode = params[:view_mode] || 'edit'
   end
 
   # POST /my_guides
   def create
     @my_guide = MyGuide.new(my_guide_params)
-    if @my_guide.save
-      redirect_to @my_guide, notice: "My Guide was successfully created."
-    else
-      render :new, status: :unprocessable_entity
+    @my_guide.user = current_user
+    respond_to do |format|
+      if @my_guide.save
+        format.html { redirect_to summary_my_guide_path(@my_guide), notice: "My Guide was successfully created." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
     end
   end
 
@@ -28,11 +34,22 @@ class MyGuidesController < ApplicationController
 
   # PATCH/PUT /my_guides/1
   def update
-    if @my_guide.update(my_guide_params)
-      redirect_to @my_guide, notice: "My_guide was successfully updated."
-    else
-      render :edit, status: :unprocessable_entity
+    respond_to do |format|
+      if @my_guide.update(my_guide_params)
+        format.html { redirect_to summary_my_guide_path(@my_guide), notice: "My Guide was successfully created." }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
     end
+  end
+
+  def summary
+  end
+
+  def download
+    @my_guide = MyGuide.find(params[:id])
+    pdf = generate_pdf(@my_guide)
+    send_data pdf.render, filename: "my_guide_#{params[:id]}.pdf", type: "application/pdf", disposition: "inline"
   end
 
   def redirect
@@ -59,16 +76,57 @@ class MyGuidesController < ApplicationController
 
   def my_guide_params
     params.require(:my_guide).permit(
-      :occupation,
-      :address,
-      :social_security,
+      :article_id,
       :business_structure,
       :is_micro_entrepreneur,
       :is_business_extension,
       :has_previous_self_employment,
       :representative_role,
-      :representative_type
+      :representative_type,
+      user_attributes: [
+        :first_name,
+        :last_name,
+        :"birthday(3i)",
+        :"birthday(2i)",
+        :"birthday(1i)",
+        :email,
+        :nationality,
+        :occupation,
+        :address,
+        :social_security,
+        :gender # Make sure to add this if you've added a gender column to your users table
+      ]
     )
   end
 
+  def generate_pdf(my_guide)
+    Prawn::Document.new do |pdf|
+      pdf.text "My Guide Summary", size: 20, style: :bold
+      pdf.move_down 10
+
+      # Assuming `business_structure` and other attributes are directly accessible
+      # on `my_guide`. Adapt these as necessary to match your actual data structure.
+      pdf.text "Business Structure: #{my_guide.business_structure}"
+      pdf.text "Micro Entrepreneur Status: #{my_guide.is_micro_entrepreneur ? 'Yes' : 'No'}"
+      pdf.text "Business Extension: #{my_guide.is_business_extension ? 'Yes' : 'No'}"
+      pdf.text "Previous Self Employment: #{my_guide.has_previous_self_employment ? 'Yes' : 'No'}"
+      pdf.text "Representative Role: #{my_guide.representative_role}"
+      pdf.text "Representative Type: #{my_guide.representative_type}"
+
+      # Accessing user information. Ensure `user` is associated with `my_guide` for this to work.
+      user = my_guide.user # This assumes a `user` association exists
+      pdf.text "User Details:", style: :bold
+      pdf.text "First Name: #{user.first_name}"
+      pdf.text "Last Name: #{user.last_name}"
+      pdf.text "Date of Birth: #{user.birthday.strftime('%d/%m/%Y') if user.birthday}"
+      pdf.text "Email: #{user.email}"
+      pdf.text "Gender: #{user.gender}"
+      pdf.text "Nationality: #{user.nationality}"
+      pdf.text "Occupation: #{user.occupation}"
+      pdf.text "Address: #{user.address}"
+      pdf.text "Social Security Number: #{user.social_security}"
+
+      pdf.render
+    end
+  end
 end
