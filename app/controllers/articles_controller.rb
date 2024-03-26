0d855230c6c49257e6071ddbd3958eb7ad1f801d
Olivier Rizk <oliv.rizk@gmail.com>
2024-03-26 17:19:58 +0100

    modifs du code de l'historique

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,17 +1,19 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: %i[show edit update destroy]
   before_action :set_sub_category, only: %i[new create]
+  before_action :session_store, only: %i[show]
 
     # GET /articles/7
   def show
     @article = Article.find(params[:id])
-    session[:store] ||= []
-    session[:store].unshift(@article.id)
-    session[:store] = session[:store].uniq
     @subcategory = @article.sub_category
     @category = @subcategory.category
   end
 
+  def session_store
+    session[:store] << params[:id] unless session[:store].include?(params[:id])
+    session[:store] = session[:store].last(5) if session[:store].size > 5
+  end
   # GET /articles/new
   def new
     @article = Article.new
