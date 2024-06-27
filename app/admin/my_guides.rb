f5354a8c95b7e1f45450d7194afd5c0421aa8146
Lea Seica <lea.seica@skema.edu>
2024-06-27 17:49:50 +0200

    Add Active Admin resources for reminders, categories, sub-categories, my guides, and platforms

---
+++ app/admin/my_guides.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register MyGuide do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :article_id, :user_id, :business_structure, :is_micro_entrepreneur, :is_business_extension, :has_previous_self_employment, :representative_role, :representative_type, :title
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:article_id, :user_id, :business_structure, :is_micro_entrepreneur, :is_business_extension, :has_previous_self_employment, :representative_role, :representative_type, :title]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
---
+++ app/admin/reminders.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Reminder do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :title, :description, :url, :status, :start_time, :end_time, :user_id, :my_guide_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:title, :description, :url, :status, :start_time, :end_time, :user_id, :my_guide_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
