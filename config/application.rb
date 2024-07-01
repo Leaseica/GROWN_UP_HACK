dd1abd8ed35d6abd342e3d3241fe7fd3a739102f
Lea Seica <lea.seica@skema.edu>
2024-07-01 16:27:09 +0200

    removed tinymce gem. Added cloud tinymce

--- config/application.rb
+++ config/application.rb
@@ -34,6 +34,6 @@ module GrownUpHack
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.beginning_of_week = :monday
-    config.tinymce.install = :copy
+
   end
 end
