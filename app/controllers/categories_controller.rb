5b7a5c83fb891d9d791ca9878eb93476893dee61
Lea Seica <lea.seica@skema.edu>
2024-07-03 16:08:53 +0200

    authenticate skip for article and cat and subcat

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-
+  before_action :authenticate_user!, except: %i[index show]
   def index
     @categories = Category.all
     if params[:query].present?
