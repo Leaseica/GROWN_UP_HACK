bbc417e3031825c90bbab8c3f3211afc6dce35ae
Lea Seica <lea.seica@skema.edu>
2024-08-02 14:47:11 +0200

    created private method for set_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,15 @@
 class UsersController < ApplicationController
 
   def show
-    @user = User.find(params[:id])
     @my_guides = @user.my_guides
     @previews_articles = session[:store]
     @reminders = Reminder.where(user: @user)
     @my_guide = MyGuide.find_by(user: @user)
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user)
   end
@@ -26,6 +22,10 @@ class UsersController < ApplicationController
 
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :occupation, :address, :birthday, :country, :phone_number, :gender, :zip_code, :city, :title, :admin, :file)
